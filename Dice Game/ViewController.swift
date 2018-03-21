d8439f1fae4dafcfe7403f3cf85b8948d0d9025d
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-03-21 22:12:12 +0300

    Code Refactoring
    Separated random number generator and image generator to a specific function and thus cleaner code

--- Dice Game/ViewController.swift
+++ Dice Game/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,10 +30,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: Any) {
+        // Pass on delegation to updateDiceImages function
+        updateDiceImages()
+    }
+    
+    func updateDiceImages () {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
-        
-        print(randomDiceIndex1)
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
